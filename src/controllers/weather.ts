fb40afe741bf35b0b70077884af7e874e6634248
Sujay Apale <apale.sujay@gmail.com>
2021-04-14 23:09:29 +0530

    feat: create axios utility and weather forecast api
    Created axios utility to send http request to external resources. Also weather api is created to
    fetch the 5 day forecast data.

---
+++ src/controllers/weather.ts
@@ -0,0 +1,58 @@
+import { NextFunction, Request, Response } from 'express';
+import { DateTime } from 'luxon';
+
+import axios from '../utils/axios';
+import { successResponse, SuccessStatusCode } from '../utils/response';
+
+const getWeatherForecast = () => async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<Response | void> => {
+	try {
+		const apiKey = 'deddd995c2a1b8347462b0c11aa747a2';
+		const latitude = '18.524766';
+		const longitude = '73.792927';
+		const location = 'Pune';
+		const unit = 'metric';
+		const lang = 'en';
+		const exclude = 'current,minutely,hourly,alerts';
+		const count = 5;
+		const queryString = `?appid=${apiKey}&lat=${latitude}&lon=${longitude}&units=${unit}&lang=${lang}&exclude=${exclude}&cnt=${count}`;
+
+		const axiosWeather = axios('weather');
+
+		const response = await axiosWeather.get(`/onecall${queryString}`);
+
+		const data = (response.data.daily as Array<any>)
+			.filter((item, index) => index < 5)
+			.map((item) => ({
+				date: DateTime.fromMillis(item.dt * 1000).toFormat(
+					'ccc LLLL dd yyyy'
+				),
+				main: item.weather[0].main,
+				description: item.weather[0].description,
+				temp: item.temp.max
+			}));
+
+		const weather = {
+			count,
+			unit,
+			location,
+			latitude,
+			longitude,
+			data
+		};
+
+		return successResponse(
+			SuccessStatusCode['200_OK'],
+			`Weather forecast from ${weather.data[0].date} to ${weather.data[4].date}`,
+			weather,
+			res
+		);
+	} catch (error) {
+		return next(error);
+	}
+};
+
+export default getWeatherForecast;
