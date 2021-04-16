5b52ca7e8d13b21e615dc720deccebe6c3cbf75f
Sujay Apale <apale.sujay@gmail.com>
2021-04-17 00:41:10 +0530

    feat: add caching functionality to the weather API

--- src/controllers/weather.ts
+++ src/controllers/weather.ts
@@ -2,8 +2,42 @@ import { NextFunction, Request, Response } from 'express';
 import { DateTime } from 'luxon';
 
 import axios from '../utils/axios';
+import { getData, setData } from '../utils/cache';
 import { successResponse, SuccessStatusCode } from '../utils/response';
 
+// Helper Function to filter and return formatted data
+const filterData = (
+	weatherData: any,
+	count: number,
+	unit: string,
+	location: string,
+	latitude: string,
+	longitude: string
+) => {
+	const data = (weatherData.daily as Array<any>)
+		.filter((item, index) => index < 5)
+		.map((item) => ({
+			date: DateTime.fromMillis(item.dt * 1000).toFormat(
+				'ccc LLLL dd yyyy'
+			),
+			main: item.weather[0].main,
+			description: item.weather[0].description,
+			temp: item.temp.max
+		}));
+
+	const weather = {
+		count,
+		unit,
+		location,
+		latitude,
+		longitude,
+		data
+	};
+
+	return weather;
+};
+
+// Controller to fetch weather forecast data
 const getWeatherForecast = () => async (
 	req: Request,
 	res: Response,
@@ -18,31 +52,51 @@ const getWeatherForecast = () => async (
 		const lang = 'en';
 		const exclude = 'current,minutely,hourly,alerts';
 		const count = 5;
-		const queryString = `?appid=${apiKey}&lat=${latitude}&lon=${longitude}&units=${unit}&lang=${lang}&exclude=${exclude}&cnt=${count}`;
+		const queryString = `?appid=${apiKey}&lat=${latitude}&lon=${longitude}&units=${unit}&lang=${lang}&exclude=${exclude}`;
 
+		// Fetch weather data from cache
+		const dataFromCache = getData(`weather:${location.toLowerCase()}`);
+
+		// If weather data found in cache return cached data to user
+		if (dataFromCache) {
+			// Filter and format the data
+			const weather = filterData(
+				dataFromCache,
+				count,
+				unit,
+				location,
+				latitude,
+				longitude
+			);
+
+			return successResponse(
+				SuccessStatusCode['200_OK'],
+				`Weather forecast from ${weather.data[0].date} to ${weather.data[4].date}`,
+				weather,
+				res
+			);
+		}
+
+		// If weather data is not found in the cache then fetch data from the weather API
 		const axiosWeather = axios('weather');
 
 		const response = await axiosWeather.get(`/onecall${queryString}`);
+		const { data } = response;
 
-		const data = (response.data.daily as Array<any>)
-			.filter((item, index) => index < 5)
-			.map((item) => ({
-				date: DateTime.fromMillis(item.dt * 1000).toFormat(
-					'ccc LLLL dd yyyy'
-				),
-				main: item.weather[0].main,
-				description: item.weather[0].description,
-				temp: item.temp.max
-			}));
+		// Save weather data in the the cache
+		if (data.daily.length !== 0) {
+			setData(`weather:${location.toLowerCase()}`, data);
+		}
 
-		const weather = {
+		// Filter and format the data
+		const weather = filterData(
+			data,
 			count,
 			unit,
 			location,
 			latitude,
-			longitude,
-			data
-		};
+			longitude
+		);
 
 		return successResponse(
 			SuccessStatusCode['200_OK'],
