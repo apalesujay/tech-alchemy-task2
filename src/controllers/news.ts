e4cbb7bc7aa0d04ee6ad502b39357144a5526069
Sujay Apale <apale.sujay@gmail.com>
2021-04-17 00:42:45 +0530

    feat: add caching functionality to the news API

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -7,7 +7,9 @@ import {
 	successResponse,
 	SuccessStatusCode
 } from '../utils/response';
+import { getData, setData } from '../utils/cache';
 
+// Controller to search news
 const getNews = () => async (
 	req: Request,
 	res: Response,
@@ -19,7 +21,9 @@ const getNews = () => async (
 		const pageSize = 20;
 		let { page } = req.query;
 		const q = req.query.search;
+		const queryString = `?q=${q}&language=${language}&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`;
 
+		// If the search query is undefined or an empty string, throw an error
 		if (!q) {
 			throw new AppError(
 				ErrorStatusCode['400_BAD_REQUEST'],
@@ -27,10 +31,12 @@ const getNews = () => async (
 			);
 		}
 
+		// If page is undefined, set the value of page to 1
 		if (page === undefined) {
 			page = '1';
 		}
 
+		// IF page number is not an integer, throw an error
 		if (
 			typeof page === 'string' &&
 			!isNaN(Number(page)) &&
@@ -43,6 +49,7 @@ const getNews = () => async (
 			);
 		}
 
+		// If page number is less than 1, throw an error
 		if (typeof page === 'string' && page < '1') {
 			throw new AppError(
 				ErrorStatusCode['400_BAD_REQUEST'],
@@ -53,6 +60,8 @@ const getNews = () => async (
 		const from = Number(page) * pageSize - pageSize + 1;
 		const to = Number(page) * pageSize;
 
+		// News API returns max 100 results for developer account
+		// If user inserts page number greater than 5, throw an error
 		if (typeof page === 'string' && page > '5') {
 			throw new AppError(
 				ErrorStatusCode['403_FORBIDDEN'],
@@ -60,12 +69,25 @@ const getNews = () => async (
 			);
 		}
 
-		const queryString = `?q=${q}&language=${language}&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`;
+		// Fetch news data from cache
+		const dataFromCache = getData(`news:${q}:${page}`);
 
+		// IF data is present in the cache, return data as response
+		if (dataFromCache) {
+			return successResponse(
+				SuccessStatusCode['200_OK'],
+				`Showing results from ${from} to ${to}`,
+				dataFromCache,
+				res
+			);
+		}
+
+		// Else fetch data from news API
 		const axiosNews = axios('news');
 
 		const response = await axiosNews.get(`/everything${queryString}`);
 
+		// Format the data
 		const data = (response.data.articles as Array<any>).map((item) => ({
 			publisher: item.source.name,
 			headline: item.title,
@@ -77,6 +99,7 @@ const getNews = () => async (
 
 		let { totalResults } = response.data;
 
+		// Since News API only returns hundred results for develoepr account, set total results to 100
 		totalResults = totalResults > 100 ? 100 : totalResults;
 
 		const news = {
@@ -85,6 +108,11 @@ const getNews = () => async (
 			searchQuery: q,
 			data
 		};
+
+		// Save data in the cache
+		if (data.length !== 0) {
+			setData(`news:${q}:${page}`, news);
+		}
 
 		return successResponse(
 			SuccessStatusCode['200_OK'],
